03f877416f31595ca5f5b77aed6403c6ae9690a0
Dima Ksenchyn <42603078+tokarkin@users.noreply.github.com>
2020-02-12 19:24:47 +0200

    Updates

---
+++ precache-manifest.ede5692a125a30c9b495760df90e780b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e74b26807f570132c18b5671ce61c8ce",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "c61cf49f0a9f2d2973fa",
+    "url": "/social-network/static/css/main.ca028018.chunk.css"
+  },
+  {
+    "revision": "d5c1993be9664e6d7d8d",
+    "url": "/social-network/static/js/2.12133893.chunk.js"
+  },
+  {
+    "revision": "c61cf49f0a9f2d2973fa",
+    "url": "/social-network/static/js/main.cff1d80f.chunk.js"
+  },
+  {
+    "revision": "89d44c35cbc314b49ac0",
+    "url": "/social-network/static/js/runtime-main.3630597a.js"
+  },
+  {
+    "revision": "4cdf3bf8e9965ecbff611cb3375adef7",
+    "url": "/social-network/static/media/Infinity-1s-270px.4cdf3bf8.svg"
+  },
+  {
+    "revision": "08f624c121a73f2e230c261f49d5376a",
+    "url": "/social-network/static/media/img.08f624c1.png"
+  },
+  {
+    "revision": "660103ff39be799858a42c2eb1e2da80",
+    "url": "/social-network/static/media/katya.660103ff.jpg"
+  },
+  {
+    "revision": "edc1b71d3d041a5b25f9e8e3e98c83a4",
+    "url": "/social-network/static/media/oleh.edc1b71d.jpg"
+  },
+  {
+    "revision": "62ac81a51fd960895836d3e0d159696b",
+    "url": "/social-network/static/media/sasha.62ac81a5.jpg"
+  },
+  {
+    "revision": "2c5d61fd2622890806e5b7a63eb08fdc",
+    "url": "/social-network/static/media/vlad.2c5d61fd.jpg"
+  }
+]);
